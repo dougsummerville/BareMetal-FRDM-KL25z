1ba1fd840f8cf04f9e10032dc5d42626371696f9
Doug Summerville <dsummer@binghamton.edu>
2020-07-08 15:11:14 -0400

    Added red led example

---
+++ drivers/redled.c
@@ -0,0 +1,21 @@
+/*Example driver for red LED on the RGBLED */
+/* Douglas Summerville, Binghamton University */
+#include <redled.h>
+#include <MKL25Z4.h>
+
+#define RED_LED_LOC 18
+
+void configure_red_led()
+{
+	SIM->SCGC5 |= SIM_SCGC5_PORTB_MASK;
+	PORTB->PCR[RED_LED_LOC] = (PORTB->PCR[RED_LED_LOC] & ~(PORT_PCR_MUX_MASK | PORT_PCR_IRQC_MASK)) | PORT_PCR_MUX(1);
+	PTB->PDDR |= (1<<RED_LED_LOC);
+}
+void turn_on_red_led()
+{
+	PTB->PCOR |= (1<<RED_LED_LOC);
+}
+void turn_off_red_led()
+{
+	PTB->PSOR |= (1<<RED_LED_LOC);
+}
---
+++ src/testredled.c
@@ -0,0 +1,8 @@
+#include <redled.h>
+
+void main()
+{
+	configure_red_led();
+	turn_on_red_led();
+	while(1);
+}
